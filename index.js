ebf17ddc82e21a8fa5bec008940a3c68e2331729
ShobhitSingh05 <143070992+ShobhitSingh05@users.noreply.github.com>
2023-08-24 13:33:20 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+import { MinHeap, MaxHeap } from '@datastructures-js/heap';
+
+const kThSmallest = (arr, n, k) => {
+  let maxyHeap = new MinHeap([]);
+
+  for (let i = 0; i < n; i++) {
+    maxyHeap.push(arr[i]);
+
+    if (maxyHeap.size() > k) {
+      maxyHeap.pop();
+    }
+  }
+
+  return maxyHeap.top();
+};
+
+console.log(kThSmallest([7, 2, 3, 5, 8, 9], 6, 3));
